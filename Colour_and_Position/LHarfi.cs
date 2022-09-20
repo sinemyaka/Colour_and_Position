3b50d332837fd6d8bc0cf2aea4b044589bc614e0
Sinem Yaka <sinemyaka35@gmail.com>
2022-09-21 00:20:13 +0300

    Initial commit

---
+++ Colour_and_Position/LHarfi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colour_and_Position
+{
+    internal class LHarfi
+    {
+        public int Xkoordinat;
+        public int Ykoordinat;
+        public int yukseklik;
+        public int genislik;
+
+        public LHarfi()
+        {
+            Xkoordinat = Convert.ToInt32(Console.ReadLine());
+            Ykoordinat = Convert.ToInt32(Console.ReadLine()); ;
+            yukseklik = Convert.ToInt32(Console.ReadLine()); ;
+            genislik = Convert.ToInt32(Console.ReadLine()); ;
+        }
+        public void Goster()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+       
+            for (int i = 0; i < yukseklik; i++)
+            {
+                Console.SetCursorPosition(Xkoordinat, Ykoordinat+i);
+                Console.WriteLine("  ");
+            }
+            
+            for (int i = 0; i < genislik; i++)
+            {
+                Console.SetCursorPosition(Xkoordinat+i, Ykoordinat+yukseklik);
+                Console.Write("   ");
+            }
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Colour_and_Position/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Colour_and_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LHarfi harf = new LHarfi();
+            while(true)
+            {
+                //harf.Xkoordinat = Convert.ToInt32(Console.ReadLine());
+                harf.Goster();
+                
+                ConsoleKeyInfo tus = Console.ReadKey();
+                if (tus.Key == ConsoleKey.A)
+                   harf.Xkoordinat--;
+                if (tus.Key == ConsoleKey.D)
+                    harf.Xkoordinat++;
+                if (tus.Key == ConsoleKey.W)
+                    harf.Ykoordinat--;
+                if (tus.Key == ConsoleKey.S)
+                    harf.Ykoordinat++;
+
+            }
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
